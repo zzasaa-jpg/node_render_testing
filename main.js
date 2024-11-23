0db97aafa6bd70df547ebc954612beafbcc6a705
[Mahendra Kumar] <[zzsdrt354@gmail.com]>
2024-11-23 17:21:55 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = 9090;
+
+app.use(express.json());
+
+let users = [];
+
+function createUser() {
+    let char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let newChar = [];
+
+    for (let i = 0; i < 5; i++) {
+        let b = Math.floor(Math.random() * 25);
+        // console.log(char[b]);
+        newChar.push(char[b]);
+    }
+    // console.log(newChar.join(''));
+    return newChar.join('');
+}
+
+for (let index = 0; index <= 100; index++) {
+    let newName = createUser();
+
+    while (users.some(user => users.name === newName)) {
+        newName = createUser();
+    }
+    users.push({ id: index, name: newName })
+}
+console.log(users);
+
+app.get('/', (req, res) => {
+    res.json(users)
+})
+
+app.listen(port, () => {
+    console.log(`${port} running`);
+})
